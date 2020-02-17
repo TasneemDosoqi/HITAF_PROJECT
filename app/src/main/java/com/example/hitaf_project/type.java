651dfe4ee737c0bf43f4e73ad43ab231f88f7a8c
TasneemDosoqi <46675287+TasneemDosoqi@users.noreply.github.com>
2020-02-17 18:24:50 +0300

    saying interface

--- app/src/main/java/com/example/hitaf_project/type.java
+++ app/src/main/java/com/example/hitaf_project/type.java
@@ -1,5 +1,6 @@
 package com.example.hitaf_project;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,7 @@ public class type extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
     }
 }
