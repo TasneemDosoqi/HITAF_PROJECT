a1087b51e097fb0e8cdbf91cdc382becc71f9618
Khawlah <KhawlahB@users.github.com>
2020-02-17 19:39:18 +0300

    done

--- app/src/main/java/com/example/hitaf_project/Home.java
+++ app/src/main/java/com/example/hitaf_project/Home.java
@@ -1,7 +1,9 @@
 package com.example.hitaf_project;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,10 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
     }
+// just for trying
+public void click(View view){
+    Intent i=new Intent(this, SignUp.class);
+    startActivity(i);
+}
 }
